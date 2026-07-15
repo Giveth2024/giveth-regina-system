8c287fa61ef21fef57c132d1d760fcbf321fc6b1
Giveth2024 <givendarian@gmail.com>
2026-07-15 19:36:45 +0300

    Feat: Added generateInvoiceNumber and updateStockItem functions

--- Helpers/helpers.js
+++ Helpers/helpers.js
@@ -93,4 +93,77 @@ function checksIfNumber(input)
     
 }
 
-module.exports = { successResponses, errorResponse, validateFields, checksIfNumber };
+async function generateInvoiceNumber(connection) {
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
+
+    const datePart = `${year}${month}${day}`;
+
+    const [rows] = await connection.query(
+        `
+        SELECT invoice_number
+        FROM sales
+        WHERE invoice_number LIKE ?
+        ORDER BY invoice_number DESC
+        LIMIT 1
+        `,
+        [`INV-${datePart}-%`]
+    );
+
+    let sequence = 1;
+
+    if (rows.length > 0) {
+        const lastInvoice = rows[0].invoice_number;
+
+        const lastSequence = parseInt(lastInvoice.split("-")[2], 10);
+
+        sequence = lastSequence + 1;
+    }
+
+    return `INV-${datePart}-${String(sequence).padStart(4, "0")}`;
+}
+
+// Update the stock table for each sale item
+async function updateStockItem(stockId, glassStatus=null, quantity, tableRows, connection){
+
+    // console.log(tableRows[0].full_quantity);
+    if(tableRows[0].full_quantity <= quantity)
+    {
+        return {
+            success : false,
+            message : `Avaliable Stock for ${tableRows[0].item_name}: ${tableRows[0].full_quantity}\n Requested: ${quantity}`
+        }
+    }
+
+    if (glassStatus === "Drinking Here")
+    {            
+        await connection.query(`
+        UPDATE stock
+        SET
+            full_quantity = full_quantity - ?,
+            empty_quantity = empty_quantity + ?
+        WHERE id = ?
+        AND full_quantity >= ?
+    `, [quantity, quantity, stockId, quantity]);
+
+    } 
+    else
+    {
+        await connection.query(`
+            UPDATE stock
+            SET full_quantity = full_quantity - ?
+            WHERE id = ?
+            AND full_quantity >= ?
+        `, [quantity, stockId, quantity]);
+    }
+
+    return {
+        success : true
+    }
+
+}
+
+module.exports = { successResponses, errorResponse, validateFields, checksIfNumber, generateInvoiceNumber, updateStockItem };
