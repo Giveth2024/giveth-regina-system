e3b9d817f8c6aead306559aa99261169abec72bc
Giveth2024 <givendarian@gmail.com>
2026-07-16 12:47:41 +0300

    Feat: Added Auth Login Frontend Page

--- Routes/webRoutes.js
+++ Routes/webRoutes.js
@@ -3,8 +3,14 @@ const path = require('path')
 
 const router = express.Router();
 
+// Home page
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+// Auth pages
+router.get("/auth/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "auth", "login.html"));
 });
 
 module.exports = router;
