d99d0bbfa985e913a9be698975387aff05bb3981
Giveth2024 <givendarian@gmail.com>
2026-07-18 11:08:13 +0300

    Added functionality to button Add New Item in viewStock.html

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,7 @@ import { footer } from "../components/footer.js";
 
 document.getElementById("navbar").innerHTML = navbar();
 document.getElementById("footer").innerHTML = footer();
+
+document.getElementById("addItem").addEventListener("click", () => {
+    window.location.href = "/frontend/stock/add"
+});
