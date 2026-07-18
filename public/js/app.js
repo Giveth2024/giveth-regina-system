65cdc3af0af781d435802c2e599454b0f9562120
Giveth2024 <givendarian@gmail.com>
2026-07-18 11:20:07 +0300

    Removed givethApp.js and updated viewStock.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 import { navbar } from "../components/navbar.js";
 import { footer } from "../components/footer.js";
+import { renderStockTable } from "./OneTime/stockRender.js";
+import { getStock } from "./OneTime/stockApi.js";
 
 
 document.getElementById("navbar").innerHTML = navbar();
@@ -8,3 +10,7 @@ document.getElementById("footer").innerHTML = footer();
 document.getElementById("addItem").addEventListener("click", () => {
     window.location.href = "/frontend/stock/add"
 });
+
+
+const stock = await getStock();
+renderStockTable(stock);
--- public/js/givethApp.js
+++
@@ -1,5 +0,0 @@
-import { renderStockTable } from "./OneTime/stockRender.js";
-import { getStock } from "./OneTime/stockApi.js";
-
-const stock = await getStock();
-renderStockTable(stock);
